c1a0c103b47ef25204e6dfb5dcc600f85a8861ab
rashid geelani <syed.geelani@vit.edu.in>
2023-12-27 21:46:19 +0530

    bgChnager

---
+++ P17-randomColorGenerator/script.js
@@ -0,0 +1,34 @@
+const randomColor = function () {
+  const hex = '0123456789ABCDEF';
+  let color = '#';
+  for(let i=0; i < 6; i++){
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+
+let intervalID;
+
+
+const startColorChange = function () {
+  if (!intervalID) {
+    intervalID = setInterval(changeBg, 1000);
+  }
+ function changeBg(){
+  document.body.style.backgroundColor = randomColor();
+ }
+};
+
+
+
+const stopColorChange = function(){
+  clearInterval(intervalID);
+  intervalID = null;
+};
+
+
+document.getElementById('start').addEventListener('click', startColorChange);
+
+
+document.getElementById('stop').addEventListener('click', stopColorChange);
